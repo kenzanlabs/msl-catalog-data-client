ec6aa4de450537edcc70684b74701f582b04e479
anram88 <aramirez@kenzanmedia.com>
2016-01-13 16:48:18 -0600

    Add unit tests

---
+++ src/test/java/com/kenzan/msl/catalog/client/cassandra/query/AlbumsQueryTest.java
@@ -0,0 +1,62 @@
+package com.kenzan.msl.catalog.client.cassandra.query;
+
+import com.google.common.base.Optional;
+import com.kenzan.msl.catalog.client.TestConstants;
+import com.kenzan.msl.catalog.client.cassandra.QueryAccessor;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.atLeastOnce;
+
+public class AlbumsQueryTest {
+
+    private TestConstants tc = TestConstants.getInstance();
+
+    @Mock
+    private QueryAccessor queryAccessor;
+
+    @Before
+    public void init() {
+        queryAccessor = mock(QueryAccessor.class);
+    }
+
+    @Test
+    public void testGetAlbumsWithLimitAndFacet() {
+        AlbumsQuery.getAlbums(queryAccessor, Optional.of(tc.FACET), Optional.of(tc.LIMIT));
+        verify(queryAccessor, atLeastOnce()).getAlbumsByFacetsWithLimit(tc.FACET, tc.LIMIT);
+    }
+
+    @Test
+    public void testGetAlbumsWithFacet() {
+        AlbumsQuery.getAlbums(queryAccessor, Optional.of(tc.FACET), Optional.absent());
+        verify(queryAccessor, atLeastOnce()).getAlbumsByFacets(tc.FACET);
+    }
+
+    @Test
+    public void testGetAlbumsWithLimit() {
+        AlbumsQuery.getAlbums(queryAccessor, Optional.absent(), Optional.of(tc.LIMIT));
+        verify(queryAccessor, atLeastOnce()).getAlbumsWithLimit(tc.LIMIT);
+    }
+
+    @Test
+    public void testGetAlbums() {
+        AlbumsQuery.getAlbums(queryAccessor, Optional.absent(), Optional.absent());
+        verify(queryAccessor, atLeastOnce()).getAlbums();
+    }
+
+    @Test
+    public void testGetAlbumArtistBySongWithLimit() {
+        AlbumsQuery.getAlbumArtistBySong(queryAccessor, tc.SONG_ID, Optional.of(tc.LIMIT));
+        verify(queryAccessor, atLeastOnce()).albumArtistBySongWithLimit(tc.SONG_ID, tc.LIMIT);
+    }
+
+    @Test
+    public void testGetAlbumArtistBySong() {
+        AlbumsQuery.getAlbumArtistBySong(queryAccessor, tc.SONG_ID, Optional.absent());
+        verify(queryAccessor, atLeastOnce()).albumArtistBySong(tc.SONG_ID);
+    }
+
+}
---
+++ src/test/java/com/kenzan/msl/catalog/client/cassandra/query/PaginationQueryTest.java
@@ -0,0 +1,81 @@
+package com.kenzan.msl.catalog.client.cassandra.query;
+
+import com.datastax.driver.mapping.Mapper;
+import com.datastax.driver.mapping.MappingManager;
+import com.google.common.base.Optional;
+import com.kenzan.msl.catalog.client.TestConstants;
+import com.kenzan.msl.catalog.client.dao.PagingStateDao;
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ MappingManager.class, Mapper.class })
+public class PaginationQueryTest {
+
+    TestConstants tc = TestConstants.getInstance();
+
+    private MappingManager manager;
+    private Mapper<PagingStateDao> mapper;
+
+    @Before
+    public void init() {
+        manager = PowerMockito.mock(MappingManager.class);
+        mapper = PowerMockito.mock(Mapper.class);
+        PowerMockito.when(manager.mapper(PagingStateDao.class)).thenReturn(mapper);
+    }
+
+    @Test
+    public void testAdd() {
+        PaginationQuery.add(manager, tc.PAGING_STATE);
+        verify(mapper, atLeastOnce()).save(eq(tc.PAGING_STATE), anyObject());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testAddException() {
+        PaginationQuery.add(null, tc.PAGING_STATE);
+    }
+
+    @Test
+    public void testGet() {
+        PowerMockito.when(mapper.get(any(UUID.class))).thenReturn(tc.PAGING_STATE);
+        Optional<PagingStateDao> result = PaginationQuery.get(manager, tc.PAGING_ID);
+        verify(mapper, atLeastOnce()).get(tc.PAGING_ID);
+        assertEquals(result.get(), tc.PAGING_STATE);
+    }
+
+    @Test
+    public void testGetNullPagingState() {
+        Optional<PagingStateDao> result = PaginationQuery.get(manager, tc.PAGING_ID);
+        PowerMockito.when(mapper.get(tc.PAGING_ID)).thenReturn(null);
+        verify(mapper, atLeastOnce()).get(tc.PAGING_ID);
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void testRemove() {
+        PaginationQuery.remove(manager, tc.PAGING_ID);
+        verify(mapper, atLeastOnce()).delete(tc.PAGING_ID);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testRemoveException() {
+        PaginationQuery.remove(null, tc.PAGING_ID);
+    }
+
+}
