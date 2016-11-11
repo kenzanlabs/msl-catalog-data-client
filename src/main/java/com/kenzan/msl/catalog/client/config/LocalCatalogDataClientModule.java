2fe8abe9b3f5e77d9cefcc9cce4f54e72d3446c4
anram88 <aramirez@kenzanmedia.com>
2016-11-11 10:55:55 -0700

    Replaced stub module for impl on local data client module

--- src/main/java/com/kenzan/msl/catalog/client/config/LocalCatalogDataClientModule.java
+++ src/main/java/com/kenzan/msl/catalog/client/config/LocalCatalogDataClientModule.java
@@ -6,8 +6,11 @@ import com.datastax.driver.mapping.MappingManager;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.name.Names;
+import com.kenzan.msl.catalog.client.cassandra.query.PaginationQuery;
 import com.kenzan.msl.catalog.client.services.CatalogDataClientService;
-import com.kenzan.msl.catalog.client.services.CatalogDataClientServiceStub;
+import com.kenzan.msl.catalog.client.services.CatalogDataClientServiceImpl;
+import com.netflix.config.DynamicIntProperty;
 import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.config.DynamicStringProperty;
 import org.codehaus.plexus.util.StringUtils;
@@ -25,16 +28,20 @@ public class LocalCatalogDataClientModule extends AbstractModule {
   private static final String DEFAULT_MSL_KEYSPACE = "msl";
   private static final String DEFAULT_MSL_REGION = "us-west-2";
   private static final String DEFAULT_CLUSTER = "127.0.0.1";
+  private static final int DEFAULT_TTL_SECS = 60 * 60; // 1 hour;
 
   private DynamicStringProperty keyspace = DynamicPropertyFactory.getInstance().getStringProperty("keyspace", DEFAULT_MSL_KEYSPACE);
   private DynamicStringProperty domain = DynamicPropertyFactory.getInstance().getStringProperty("domain", DEFAULT_CLUSTER);
   private DynamicStringProperty region = DynamicPropertyFactory.getInstance().getStringProperty("region", DEFAULT_MSL_REGION);
+  private static DynamicIntProperty ttlSecs = DynamicPropertyFactory.getInstance().getIntProperty("paging_state_ttl_secs", DEFAULT_TTL_SECS);
 
   private final static Logger LOGGER = LoggerFactory.getLogger(CatalogDataClientModule.class);
 
   @Override
   protected void configure() {
-    bind(CatalogDataClientService.class).to(CatalogDataClientServiceStub.class).asEagerSingleton();
+    bindConstant().annotatedWith(Names.named("ttlSecs")).to(ttlSecs.getValue());
+    requestStaticInjection(PaginationQuery.class);
+    bind(CatalogDataClientService.class).to(CatalogDataClientServiceImpl.class).asEagerSingleton();
   }
 
   @Provides
